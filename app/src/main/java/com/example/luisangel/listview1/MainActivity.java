54b8a3d34babfbef18cf4b89d33fd3c3551cde24
luisaezmese <luisaezmese@gmail.com>
2017-01-31 21:05:05 +0100

    Listerner sobre ListView

--- app/src/main/java/com/example/luisangel/listview1/MainActivity.java
+++ app/src/main/java/com/example/luisangel/listview1/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.luisangel.listview1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
         final ListView listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new nuevoListener());
+
+    }
+
+    private class nuevoListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            String elemento = (String)parent.getItemAtPosition(position);
+
+            Toast.makeText(MainActivity.this,elemento,Toast.LENGTH_LONG).show();
+        }
 
     }
 }
